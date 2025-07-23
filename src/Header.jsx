ac65c847b15d93b085b3cc241562001c5481c06b
Mahfuzshihab <mahfuzshihab.fin@hotmail.com>
2025-07-23 04:32:10 +0300

    Frontend | Backend |

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,24 +1,38 @@
-import { Link } from 'react-router-dom';
-import './styles/Header.css';
+import { Link } from "react-router-dom";
+import "./styles/Header.css";
 
 const Header = () => {
-    return (
-        <header className="header">
-            <div className="logo-sec">
-                <h1 className="logo">HR App</h1>
-            </div>
+  return (
+    <header className="header">
+      <div className="logo-sec">
+        <h1 className="logo">HR App</h1>
+      </div>
 
-            <nav className="nav">
-                <ul>
-                    <li><Link to="/">Employees</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/add">Add Employee</Link></li>
-                    <li><a href="http://localhost:3001/employees" target="_blank" rel="noopener noreferrer">Backend Info</a></li>
-
-                </ul>
-            </nav>
-        </header>
-    );
+      <nav className="nav">
+        <ul>
+          <li>
+            <Link to="/">Employees</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/add">Add Employee</Link>
+          </li>
+          <li>
+            {" "}
+            <a
+              href="https://hr-app-backend-ebon.vercel.app/api/employees.js"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Backend Info
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
